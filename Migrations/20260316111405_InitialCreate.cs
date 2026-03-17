0ed428dd49b83f7c79a910eae58a4ee64ff1db05
streak <tery581@gmail.com>
2026-03-17 09:52:57 +0200

    Add files via upload

--- Migrations/20260316111405_InitialCreate.cs
+++ Migrations/20260316111405_InitialCreate.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Carsweb.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -59,7 +57,6 @@ namespace Carsweb.Migrations
                 });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
